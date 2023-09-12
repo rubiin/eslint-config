345edb6e6b8714c8a7ce5ac232c8f701b00126a2
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-09-12 19:30:39 +0545

    fix: add type checked rules

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,4 +1,4 @@
-import { existsSync } from "fs";
+const { existsSync } = require("fs");
 // Determine whether the project has a tsconfig.json file
 const hasTSConfig = existsSync("tsconfig.json");
 
