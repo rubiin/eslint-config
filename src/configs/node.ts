40bcc0936daf4700e732a4c959fc1de632a5caf6
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-09-26 08:35:36 +0545

    fix: disable no buffer and process

--- src/configs/node.ts
+++ src/configs/node.ts
@@ -1,5 +1,6 @@
 import type { FlatESLintConfigItem } from "eslint-define-config"
 import { pluginNode } from "../plugins"
+import { OFF } from "../flags"
 
 export const node: FlatESLintConfigItem[] = [
   {
@@ -12,8 +13,8 @@ export const node: FlatESLintConfigItem[] = [
       "node/no-exports-assign": "error",
       "node/no-new-require": "error",
       "node/no-path-concat": "error",
-      "node/prefer-global/buffer": ["error", "never"],
-      "node/prefer-global/process": ["error", "never"],
+      "node/prefer-global/buffer": OFF,
+      "node/prefer-global/process": OFF,
       "node/process-exit-as-throw": "error",
     },
   },
